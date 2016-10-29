9c9a684e92a7a4e5f8aa40ff2cc479005f019722
邓斌 <dengbin@dengbindeMacBook-Pro.local>
2016-10-29 22:52:58 +0800

    first version
    first version

---
+++ src/main/java/com/struts/learn/TestAction.java
@@ -0,0 +1,37 @@
+package com.struts.learn;
+
+import java.io.UnsupportedEncodingException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts2.ServletActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class TestAction extends ActionSupport {
+
+	private static final long serialVersionUID = 1L;
+
+	public String execute(){
+		return SUCCESS;
+	}
+	
+	public String login() {
+		try {
+			HttpServletRequest request = ServletActionContext.getRequest();
+			HttpServletResponse response = ServletActionContext.getResponse();
+			request.setCharacterEncoding("UTF-8");
+			response.setContentType("text/html;charset=utf-8");
+			String username = request.getParameter("username");
+			String password = request.getParameter("password");
+	                //页面跳转
+			if ("admin".equals(username) && "123456".equals(password)) {
+				return SUCCESS;
+			} else {
+				return "login";
+			}
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		return SUCCESS;
+	}
+}
